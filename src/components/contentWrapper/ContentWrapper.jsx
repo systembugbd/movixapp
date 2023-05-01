ba1a925a86e91fa1e597c7e926b63ef27db701d4
Md. Shaheb Ali <shahebali247bd@gmail.com>
2023-05-01 11:05:45 +0600

    Hero Section done

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContentWrapper({ children }) {
+      return <div className="contentWrapper">{children}</div>;
+}
+
+export default ContentWrapper;
--- src/pages/home/heroBanner/heroBanner.jsx
+++ src/pages/home/heroBanner/heroBanner.jsx
@@ -3,23 +3,36 @@ import "./style.scss";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector, useDispatch } from "react-redux";
+import Img from "../../../components/lazyLoadImage/img";
+import ContentWrapper from "./../../../components/contentWrapper/ContentWrapper";
 
 function heroBanner() {
       const [background, setBackground] = useState("");
       const [query, setQuery] = useState("");
+      const [title, setTitle] = useState("");
       const navigate = useNavigate();
-      const baseUrlWithSize = useSelector((state) => state.home.url.backdrop);
+      const url = useSelector((state) => state.home.url);
 
       //Fetch upcomming movie data
       const { data, loading } = useFetch("/movie/upcoming");
 
       useEffect(() => {
-            const bgPath =
+            const popularBgURL =
+                  url?.backdrop +
                   data?.results?.[Math.floor(Math.random() * 20)]
                         ?.backdrop_path;
-            setBackground(bgPath);
+            setBackground(popularBgURL);
+
+            setTitle(data?.results?.[Math.floor(Math.random() * 20)]?.title);
       }, [data]);
 
+      //OnSubmit Handler
+      const onSubmitHandler = () => {
+            if (query.length > 0) {
+                  navigate(`/search/${query}`);
+            }
+      };
+      //on Enter Key press Handler
       const searchInputHandler = (e) => {
             if (e.key == "Enter" && query.length > 0) {
                   navigate(`/search/${query}`);
@@ -28,29 +41,36 @@ function heroBanner() {
 
       return (
             <div className="heroBanner">
-                  <div className="heroBGImage">
-                        <img src={baseUrlWithSize + background} />
-                  </div>
-                  <div className="wrapper">
+                  {!loading && (
+                        <div className="backdrop-img">
+                              <Img src={background} title={title} />
+                        </div>
+                  )}
+                  <div className="gradient-layer"></div>
+                  <ContentWrapper>
                         <div className="heroBannerContent">
                               <span className="title">Welcome</span>
                               <span className="subTitle">
-                                    All kinds of Movie and TV show with details
+                                    Millions of movie and TV show and people to
+                                    discover, Explore Now
                               </span>
                               <div className="searchInput">
                                     <input
                                           className="search"
                                           type="text"
                                           value={query}
+                                          placeholder="Type your query here..."
                                           onChange={(event) =>
                                                 setQuery(event.target.value)
                                           }
                                           onKeyUp={searchInputHandler}
                                     />
-                                    <button>Search</button>
+                                    <button onClick={onSubmitHandler}>
+                                          Search
+                                    </button>
                               </div>
                         </div>
-                  </div>
+                  </ContentWrapper>
             </div>
       );
 }
